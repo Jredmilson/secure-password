314791e59c254f531ec005328164102cfefb6fdb
unknown <jredmilsonn@gmail.com>
2024-10-12 15:12:43 -0300

    Criação do projeto

---
+++ src/test/java/com/jred/ed/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jred.ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
